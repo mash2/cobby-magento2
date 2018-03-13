a157d9b6e2b76635cc7fe5a553fac16951c5aba2
sl4d1c <slawa.minden@freenet.de>
2018-03-13 09:43:07 +0100

    saving progress so far

--- Controller/Test/Sayhello.php
+++ Controller/Test/Sayhello.php
@@ -8,8 +8,21 @@
 
 namespace Mash2\Cobby\Controller\Test;
 
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Filesystem;
+
 class Sayhello extends \Magento\Framework\App\Action\Action
 {
+    protected $_filesystem;
+
+    public function __construct(
+        Context $context,
+        \Magento\Framework\Filesystem $_filesystem
+    ){
+        parent::__construct($context);
+        $this->_filesystem = $_filesystem;
+    }
+
     /**
      * say hello text
      */
@@ -21,10 +34,14 @@ class Sayhello extends \Magento\Framework\App\Action\Action
         }
     }
 
-    public function getImage() {
-        $image = $this->getRequest()->getParam('image');
+    private function getImage()
+    {
 
-        $file = "/var/www/html/pub/media/import/tasche.jpg";
+        $id = $this->getRequest()->getParam('id');
+        $filename = $this->getRequest()->getParam('filename');
+
+        $filePath = $this->getImagePath($id, $filename);
+
         $type = 'image/jpeg';
 
 //        header('Content-Type: image/jpeg');
@@ -38,12 +55,47 @@ class Sayhello extends \Magento\Framework\App\Action\Action
         $this->getResponse()->sendHeaders();
 
         $ioAdapter = new \Magento\Framework\Filesystem\Io\File();
-        $ioAdapter->open(array('path' => $ioAdapter->dirname($file)));
+        $ioAdapter->open(array('path' => $ioAdapter->dirname($filePath)));
 //        $ioAdapter->streamOpen($file, 'r');
         while ($buffer = $ioAdapter->streamRead()) {
             print $buffer;
         }
         $ioAdapter->streamClose();
+    }
 
+    private function getImagePath($id ,$filename)
+    {
+        $dirConfig = DirectoryList::getDefaultConfig();
+        $dirAddon = $dirConfig[DirectoryList::MEDIA][DirectoryList::PATH];
+        $baseMediaUrl = $this->_filesystem->getDirectoryRead(DirectoryList::MEDIA)->getAbsolutePath();
+        $baseMediaDir = DirectoryList::MEDIA;
+
+        if ((int)$id){
+            $product = Mage::getModel('catalog/product')->load($id);
+            if ($product->getId()) {
+                foreach ($product->getMediaGallery('images') as $image) {
+                    $tokens = explode('/', $image['file']);
+                    $str = trim(end($tokens));
+                    if ($str == $filename){
+                        $file = Mage::helper('catalog/image')->init($product, 'thumbnail', $image['file']);
+                        $fileString = (string)$file;
+                        $filePath = str_replace($baseMediaUrl, $baseMediaDir, $fileString);
+
+                        return $filePath;
+                    }
+                }
+            }
+        } else if (file_exists($baseMediaDir . 'import/'. $filename)) {
+            $filePath = $baseMediaDir . 'import/' . $filename;
+
+            return $filePath;
+        }
+
+//        $placeholder = Mage::getDesign()->getSkinUrl('images/catalog/product/placeholder/thumbnail.jpg');
+//        $baseSkinUrl = Mage::getBaseUrl('skin');
+//        $baseSkinDir = Mage::getBaseDir() . DS . 'skin' . DS;
+//        $filePath = str_replace($baseSkinUrl, $baseSkinDir, $placeholder);
+
+//        return $filePath;
     }
 }
