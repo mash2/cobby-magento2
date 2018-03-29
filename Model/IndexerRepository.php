2d96ed1b8d8201f4401005bffab8c59b26344003
Dima Janzen <dima.janzen@gmail.com>
2018-03-29 15:54:11 +0200

    COBBY-3410 added support for catalog_product_price, catalog_category_product, cataloginventory_stock index

--- Model/IndexerRepository.php
+++ Model/IndexerRepository.php
@@ -20,7 +20,7 @@ class IndexerRepository implements \Mash2\Cobby\Api\IndexerRepositoryInterface
     /**
      * @var \Magento\Catalog\Model\Indexer\Product\Flat\Processor
      */
-    protected $flatProcessor;
+    protected $productFlatProcessor;
 
     /**
      * @var \Magento\Framework\Json\Helper\Data
@@ -38,9 +38,27 @@ class IndexerRepository implements \Mash2\Cobby\Api\IndexerRepositoryInterface
     protected $categoryFlatState;
 
     /**
+     * @var \Magento\Catalog\Model\Indexer\Category\Product
+     */
+    private $categoryProductProcessor;
+
+    /**
+     * @var \Magento\Catalog\Model\Indexer\Product\Price
+     */
+    private $priceProductProcessor;
+
+    /**
+     * @var \Magento\CatalogInventory\Model\Indexer\Stock
+     */
+    private $stockProductProcessor;
+
+    /**
      * @param \Magento\Indexer\Model\Indexer\Collection $indexerCollection
      * @param \Magento\Indexer\Model\Indexer $indexer
-     * @param \Magento\Catalog\Model\Indexer\Product\Flat\Processor $flatProcessor
+     * @param \Magento\Catalog\Model\Indexer\Product\Flat\Processor $productFlatProcessor
+     * @param \Magento\Catalog\Model\Indexer\Category\Product $categoryProductProcessor
+     * @param \Magento\Catalog\Model\Indexer\Product\Price $priceProductProcessor
+     * @param \Magento\CatalogInventory\Model\Indexer\Stock $stockProductProcessor
      * @param \Magento\Framework\Json\Helper\Data $jsonHelper
      * @param \Magento\Catalog\Model\Indexer\Product\Flat\State $productFlatState
      * @param \Magento\Catalog\Model\Indexer\Category\Flat\State $categoryFlatState
@@ -48,7 +66,10 @@ class IndexerRepository implements \Mash2\Cobby\Api\IndexerRepositoryInterface
     public function __construct(
         \Magento\Indexer\Model\Indexer\Collection $indexerCollection,
         \Magento\Indexer\Model\Indexer $indexer,
-        \Magento\Catalog\Model\Indexer\Product\Flat\Processor $flatProcessor,
+        \Magento\Catalog\Model\Indexer\Product\Flat\Processor $productFlatProcessor,
+        \Magento\Catalog\Model\Indexer\Category\Product $categoryProductProcessor,
+        \Magento\Catalog\Model\Indexer\Product\Price $priceProductProcessor,
+        \Magento\CatalogInventory\Model\Indexer\Stock $stockProductProcessor,
         \Magento\Framework\Json\Helper\Data $jsonHelper,
         \Magento\Catalog\Model\Indexer\Product\Flat\State $productFlatState,
         \Magento\Catalog\Model\Indexer\Category\Flat\State $categoryFlatState
@@ -56,10 +77,13 @@ class IndexerRepository implements \Mash2\Cobby\Api\IndexerRepositoryInterface
     {
         $this->indexerCollection = $indexerCollection;
         $this->indexer = $indexer;
-        $this->flatProcessor = $flatProcessor;
+        $this->productFlatProcessor = $productFlatProcessor;
         $this->jsonHelper = $jsonHelper;
         $this->productFlatState = $productFlatState;
         $this->categoryFlatState = $categoryFlatState;
+        $this->categoryProductProcessor = $categoryProductProcessor;
+        $this->priceProductProcessor = $priceProductProcessor;
+        $this->stockProductProcessor = $stockProductProcessor;
     }
 
     public function export()
@@ -95,9 +119,28 @@ class IndexerRepository implements \Mash2\Cobby\Api\IndexerRepositoryInterface
 
         $result = false;
 
-        if ($indexer == 'catalog_product_flat' && (count($productIds) > 0) ) {
-            $this->flatProcessor->reindexList($productIds, true);
-            $result = true;
+        if (!count($productIds)) {
+            return $result;
+        }
+
+        switch ($indexer)
+        {
+            case 'cataloginventory_stock':
+                $this->stockProductProcessor->executeList($productIds);
+                $result = true;
+                break;
+            case 'catalog_product_flat':
+                $this->productFlatProcessor->reindexList($productIds, true);
+                $result = true;
+                break;
+            case 'catalog_category_product':
+                $this->categoryProductProcessor->executeList($productIds);
+                $result = true;
+                break;
+            case 'catalog_product_price':
+                $this->priceProductProcessor->executeList($productIds);
+                $result = true;
+                break;
         }
 
         return $result;
