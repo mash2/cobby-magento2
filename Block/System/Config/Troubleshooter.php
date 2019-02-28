5e68d004993b05079f7d05fb0f71d108f14fa5ba
Slavko Dick <slavko.dick@mash2.com>
2019-02-28 13:05:15 +0100

    init

---
+++ Block/System/Config/Troubleshooter.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Mash2\Cobby\Block\System\Config;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class Troubleshooter extends Field
+{
+    /**
+     * @var string
+     */
+
+    protected $_template = 'Mash2_Cobby::system/config/troubleshooter.phtml';
+
+    /**
+     * @param Context $context
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Remove scope label
+     *
+     * @param  AbstractElement $element
+     * @return string
+     */
+    public function render(AbstractElement $element)
+    {
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+        return parent::render($element);
+    }
+
+    /**
+     * Return element html
+     *
+     * @param  AbstractElement $element
+     * @return string
+     */
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        return $this->_toHtml();
+    }
+
+    /**
+     * Return ajax url for collect button
+     *
+     * @return string
+     */
+    public function getAjaxUrl()
+    {
+        return $this->getUrl('mash2_cobby/system_config/troubleshooter');
+    }
+
+    /**
+     * Generate collect button html
+     *
+     * @return string
+     */
+    public function getButtonHtml()
+    {
+        $button = $this->getLayout()->createBlock(
+            'Magento\Backend\Block\Widget\Button'
+        )->setData(
+            [
+                'id' => 'collect_button',
+                'label' => __('Go!'),
+            ]
+        );
+
+        return $button->toHtml();
+    }
+}
