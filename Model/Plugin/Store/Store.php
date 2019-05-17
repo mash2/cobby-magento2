c6a825a14b6b4295614ec012f34e69400385c381
slavko dick <slavko.dick@mash2.com>
2019-05-17 13:31:28 +0200

    changed form observer to plugin

--- Model/Plugin/Store/Store.php
+++ Model/Plugin/Store/Store.php
@@ -4,6 +4,21 @@ namespace Mash2\Cobby\Model\Plugin\Store;
 
 class Store extends \Mash2\Cobby\Model\Plugin\AbstractPlugin
 {
+    private $state;
+    private $messageManager;
+
+    public function __construct(
+        \Mash2\Cobby\Helper\Queue $queueHelper,
+        \Mash2\Cobby\Model\ProductFactory $productFactory,
+        \Magento\Indexer\Model\Indexer\State $state,
+        \Magento\Framework\Message\ManagerInterface $messageManager
+    ){
+        $this->state = $state;
+        $this->messageManager = $messageManager;
+
+        parent::__construct($queueHelper,$productFactory);
+    }
+
     public function aroundSave(
         \Magento\Store\Model\ResourceModel\Store $storeResource,
         \Closure $proceed,
@@ -12,6 +27,8 @@ class Store extends \Mash2\Cobby\Model\Plugin\AbstractPlugin
         $storeResource->addCommitCallback(function () use ($store) {
             $this->resetHash('store_changed');
         });
+
+        $this->state->setStatus(\Magento\Framework\Indexer\StateInterface::STATUS_INVALID);
 
         return $proceed($store);
     }
@@ -22,6 +39,8 @@ class Store extends \Mash2\Cobby\Model\Plugin\AbstractPlugin
         \Magento\Framework\Model\AbstractModel $store
     ){
         $this->resetHash('store_changed');
+
+        $this->state->setStatus(\Magento\Framework\Indexer\StateInterface::STATUS_INVALID);
 
         return $proceed($store);
     }
--- Model/Plugin/Store/Website.php
+++ Model/Plugin/Store/Website.php
@@ -3,6 +3,21 @@ namespace Mash2\Cobby\Model\Plugin\Store;
 
 class Website extends \Mash2\Cobby\Model\Plugin\AbstractPlugin
 {
+    private $state;
+    private $messageManager;
+
+    public function __construct(
+        \Mash2\Cobby\Helper\Queue $queueHelper,
+        \Mash2\Cobby\Model\ProductFactory $productFactory,
+        \Magento\Indexer\Model\Indexer\State $state,
+        \Magento\Framework\Message\ManagerInterface $messageManager
+    ){
+        $this->state = $state;
+        $this->messageManager = $messageManager;
+
+        parent::__construct($queueHelper,$productFactory);
+    }
+
     public function aroundSave(
         \Magento\Store\Model\ResourceModel\Website $websiteResource,
         \Closure $proceed,
@@ -11,6 +26,8 @@ class Website extends \Mash2\Cobby\Model\Plugin\AbstractPlugin
         $websiteResource->addCommitCallback(function () use ($website) {
            $this->resetHash('website_changed');
         });
+
+        $this->state->setStatus(\Magento\Framework\Indexer\StateInterface::STATUS_INVALID);
 
         return $proceed($website);
     }
@@ -21,6 +38,8 @@ class Website extends \Mash2\Cobby\Model\Plugin\AbstractPlugin
         \Magento\Framework\Model\AbstractModel $website
     ){
         $this->resetHash('website_changed');
+
+        $this->state->setStatus(\Magento\Framework\Indexer\StateInterface::STATUS_INVALID);
 
         return $proceed($website);
     }
